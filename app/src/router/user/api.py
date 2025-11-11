12c5bf033e20ee34e03a3d5994aaab502755f3e2
mra-project <mra.project@proton.me>
2025-11-11 01:52:39 +0000

    fix(endpoint): adjustment endpoint structure

--- app/src/router/user/api.py
+++ app/src/router/user/api.py
@@ -30,6 +30,18 @@ async def get_list(
         response.data = [UserBaseModel.model_validate(user) for user in users]
     return response.build()
 
+@router.get("/profile")
+async def profile(
+    authentication: dict = Depends(auth_service.require_access_token),
+    session: AsyncSession = Depends(get_async_session)
+):
+    user = await crud_user.get_user_by_id(session=session, id=authentication.get("id"))
+    with response_handler() as response:
+        response.status_code = 200
+        response.message = "Get Profile Successfully."
+        response.data = UserProfile.model_validate(user)
+    return response.build()
+
 @router.get("/{id}")
 async def profile(
     id: int,
@@ -40,17 +52,5 @@ async def profile(
     with response_handler() as response:
         response.status_code = 200
         response.message = "Get User Successfully."
-        response.data = UserProfile.model_validate(user)
-    return response.build()
-
-@router.get("/profile")
-async def profile(
-    authentication: dict = Depends(auth_service.require_access_token),
-    session: AsyncSession = Depends(get_async_session)
-):
-    user = await crud_user.get_user_by_id(session=session, id=authentication.get("id"))
-    with response_handler() as response:
-        response.status_code = 200
-        response.message = "Get Profile Successfully."
         response.data = UserProfile.model_validate(user)
     return response.build()
