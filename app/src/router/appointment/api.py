3cdd269b423d7678f05b4a83406501098cf76cdf
mra-project <mra.project@proton.me>
2025-12-17 04:37:34 +0000

    remove else condition

--- app/src/router/appointment/api.py
+++ app/src/router/appointment/api.py
@@ -36,12 +36,11 @@ async def create_appointment(
 
         if not prof:
             raise ValueError("Doctor not found.")
+        
         if prof.specialization == "Nutritionist/Dietitian" and not await crud_app.exists_this_week(session=session, user_id=user_id):
             await reward_user_points(session=session, user_id=user_id, category=CategoryCode.konseling_gizi)
         elif prof.specialization == "Psychologist" and not await crud_app.exists_this_month(session=session, user_id=user_id):
             await reward_user_points(session=session, user_id=user_id, category=CategoryCode.konseling_psikolog)
-        else:
-            raise ValueError(f"Please check specialization of {prof.fullname}")
         
         ap = await crud_app.create_appointment(
             session=session,
